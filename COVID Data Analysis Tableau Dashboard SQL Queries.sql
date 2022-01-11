51634c13f6813d5241b56bb4c8181d01eb0bbef3
Swaroop Todankar <48900719+SwoopGT@users.noreply.github.com>
2022-01-11 19:22:02 +0530

    Add files via upload

---
+++ COVID Data Analysis Tableau Dashboard SQL Queries.sql
@@ -0,0 +1,31 @@
+
+-- 1. Death Percentage
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+where continent is not null 
+order by 1,2
+
+
+-- 2. Total Death Count
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3. Percentage Population Infected
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4. Percentage Population Infected by Date
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
